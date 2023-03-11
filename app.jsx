f9cba41ac87afa53301a64557b5c12acf59e11ad
Lucas <111457694+LucasNFortes@users.noreply.github.com>
2023-03-11 19:19:24 -0400

    Add files via upload

--- app.jsx
+++ app.jsx
@@ -1,8 +1,10 @@
+import React, { useState, useEffect } from "react";
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 const navigation =
   document.querySelector(".primary-navigation");
-  
+
 const navigationHeight =
   navigation.offsetHeight;
 
@@ -11,16 +13,59 @@ document.documentElement.style.setProperty(
   navigationHeight + "px"
 );
 
-
-
-
-/*----------------------------------corrigir--------------------------------------*/
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 10) {
-    header.style.backgroundColor = '#007f78ff';
-  } else {
-    header.style.backgroundColor = '';
+/*Formulário*/
+class Formulario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Nome: '',
+      Email: '',
+      Mensagem: ''
+    };
   }
-});
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const endpoint = 'https://formsubmit.co/dryen.nascimento.galvao@gmail.com';
+    const data = new FormData();
+    data.append('Nome', this.state.Nome);
+    data.append('Email', this.state.Email);
+    data.append('Mensagem', this.state.Mensagem);
+
+    fetch(endpoint, {
+      method: 'POST',
+      body: data
+    })
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+  }
+
+  render() {
+    return (
+      <div id="my-form">
+        <div className="form">
+          <h6>MENSAGEM DIRETA</h6>
+          <h1>Mande uma mensagem</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input className="nome" name="nome" placeholder="Nome" value={this.state.nome} onChange={this.handleChange} />
+            <br />
+            <input className="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+            <br />
+            <input className="mensagem" name="mensagem" placeholder="Mensagem" value={this.state.mensagem} onChange={this.handleChange} />
+            <br />
+            <button id="send-msg" type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Formulario;
+/*End Formulário*/
